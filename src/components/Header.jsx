00ce10aeda57807f9b41805edc3fb62cd0cecb05
khazarglzd <khazarguluzade@gmail.com>
2024-09-10 15:23:15 +0400

    creating Header component &  dark / light toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Header.css"
 import { CiSearch } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CiDark } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+
 
 const Header = () => {
+
+    const [theme, setTheme] = useState(false)
+
+    const changeTheme = () => {
+        const root = document.getElementById("root")
+        if (theme) {
+            root.style.backgroundColor = "#fff"
+            root.style.color = "#000"
+        }
+        else {
+            root.style.color = "#fff"
+            root.style.backgroundColor = "#000"
+        }
+        setTheme(!theme)
+    }
+
     return (
         <div className='header-container'>
             <img className='logo' src="./src/images/logo.png" alt="" />
@@ -12,10 +32,15 @@ const Header = () => {
             </div>
             <div className='header-right-side-icons-div'>
                 <div className='theme-toggle'>
+                    {
+                        theme ? <CiLight className='icons' onClick={changeTheme} /> :
+                            <CiDark className='icons' onClick={changeTheme} />
+                    }
                 </div>
+                <CiShoppingCart className='icons' />
             </div>
         </div>
     )
 }
 
-export default Header
+export default Header 
