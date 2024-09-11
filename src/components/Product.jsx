4c2be01079f2958585e5d29b8d91524d33b3fb0e
khazarglzd <khazarguluzade@gmail.com>
2024-09-12 02:08:08 +0400

    styling ProductList & Product components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
 
+import "../css/Product.css"
 
 const Product = ({ product }) => {
 
     const { id, title, price, description, image } = product
 
-
     return (
-        <div >
+        <div className='product-cart'>
             <img src={image} alt="product image" width={120} height={150} />
-            <p >{title}</p>
-            <div >
-                <h3 >{price} $</h3>
-                <button >Details</button>
+            <p className='product-title'>{title}</p>
+            <div className='price_action'>
+                <h3 className='product-price'>{price} $</h3>
+                <button className='details'>Details</button>
             </div>
         </div>
     )
