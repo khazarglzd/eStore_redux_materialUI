8125ffddb8f4322598e5bfc783702eb583872437
khazarglzd <khazarguluzade@gmail.com>
2024-09-14 17:33:32 +0400

    adding  Loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css"
 import Header from './components/Header'
 import PageContainer from './container/PageContainer'
 import ProductList from './components/ProductList'
+import Loading from './components/Loading'
 
 const App = () => {
   return (
     <PageContainer>
+      <Loading />
       <Header />
       <ProductList />
     </PageContainer>
